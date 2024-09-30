f4890f5f4e0d12e06cf21d6c6a1ce160bf51a39a
Jihene556 <hhamoudajihene@gmail.com>
2024-09-30 21:17:31 +0200

    final version booking.py

--- booking/booking.py
+++ booking/booking.py
@@ -11,7 +11,8 @@ HOST = '0.0.0.0'
 
 with open('{}/databases/bookings.json'.format("."), "r") as jsf:
    bookings = json.load(jsf)["bookings"]
-
+   
+ 
 @app.route("/", methods=['GET'])
 def home():
    return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
@@ -31,43 +32,42 @@ def get_booking_for_user(userid):
             return res
     return make_response(jsonify({"error":"User ID not found"}),400)
  
+def write(bookings):
+    with open('{}/databases/booking.json'.format("."), 'w') as f:
+        json.dump({"bookings": bookings}, f)
 
 # Add a booking for a user 
 @app.route("/bookings/<userid>", methods=['POST'])
 def add_booking_byuser(userid):
    req = request.get_json()
-   print(req)
-   for booking in bookings:
-        if str(booking["userid"]) == str(userid):
-         if req in booking["dates"]:
-            
-            return make_response(jsonify({"error":"this booking already exists"}),409)
-         booking["dates"].append(req)
-         write(bookings)
-         res = make_response(jsonify({"message":"booking added for the requested user"}) ,200)
-         return res   
-      
-def write(bookings):
-    with open('{}/databases/booking.json'.format("."), 'w') as f:
-        json.dump({"bookings": bookings}, f)
-'''
-# Method 2 Add a booking for a user 
-@app.route("/bookings/<userid>", methods=['POST'])
-def add_booking_byuser(userid):
-   req = request.get_json()
    new_booking = {"date": req["date"], "movies": req["movies"]}
+   
+   showtime_url = f"http://127.0.0.1:3202/showmovies/{new_booking['date']}"
+   response = requests.get(showtime_url)
+   
+   if response.status_code != 200:
+      return make_response(jsonify({"error": "The specified date does not exist"}), 404)
+   showtimes_data = response.json()
+   
+   movies_available = showtimes_data["movies"]
+   
+   if new_booking["movies"] not in movies_available:
+      return make_response(jsonify({"error": f"The movie you chose is not available on the chosen date"}), 404)
+
    for booking in bookings:
         if str(booking["userid"]) == str(userid):
          for b in booking["dates"]:
             if b["date"]==new_booking["date"]:
-               if b["movies"] in new_booking["movies"]:
+               if new_booking["movies"] in b["movies"]:
                   return make_response(jsonify({"error": "This booking already exists for the specified user"}), 409)
+               else :
+                  b["movies"].append(new_booking["movies"])
          booking["dates"].append(req)
-         #write(bookings)
+         write(bookings)
          res = make_response(jsonify({"message":"booking added for the requested user"}) ,200)
          return res  
    return make_response(jsonify({"error": "User ID not found"}), 404) 
-'''
+
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
